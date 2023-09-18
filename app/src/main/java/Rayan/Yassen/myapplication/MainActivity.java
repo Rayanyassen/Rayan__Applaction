b830854834117764225f4b5d4181f49f4155a22a
Rayan <rayanyassen23@gmail.com>
2023-09-18 13:44:39 +0300

    Rayan

--- app/src/main/java/Rayan/Yassen/myapplication/MainActivity.java
+++ app/src/main/java/Rayan/Yassen/myapplication/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,8 +13,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d("yassen",'onCreate');
-
+        Log.d("yassen", "onCreate: ");
+        Toast.makeText(this,
     }
 
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+
 }
+
