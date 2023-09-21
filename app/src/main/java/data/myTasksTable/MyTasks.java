d0fd551a2f567edd9f529f1382d48868b3d3b143
Rayan <rayanyassen23@gmail.com>
2023-09-21 14:55:14 +0300

    Rayan

--- app/src/main/java/data/myTasksTable/MyTasks.java
+++ app/src/main/java/data/myTasksTable/MyTasks.java
@@ -9,11 +9,12 @@ public class MyTasks {
     @PrimaryKey(autoGenerate = true)
     public long keyid;//رقم المهمه
     @ColumnInfo(name = "textTasks")// اعطاء اسم جديد للعامود- الصفة في الجدول
-    String text;//نص المهمات
-    int importance;//درجه الاهمية 1-5
-    long subjid;//رقم موضوع المهمة
-    long userid;//رقم مستعمل الذي اضاف المهمه
-    long time;//وقت انتاج المهمه
+    public String text;//نص المهمات
+    public int importance;//درجه الاهمية 1-5
+    public long subjid;//رقم موضوع المهمة
+    public long userid;//رقم مستعمل الذي اضاف المهمه
+    public long time;// وقت انتاج المهمه
+    public boolean isCompleted ; // هل تمت المهمة
 
     public long getKeyid() {
         return keyid;
@@ -63,15 +64,24 @@ public class MyTasks {
         this.time = time;
     }
 
+    public boolean isCompleted() {
+        return isCompleted;
+    }
+
+    public void setCompleted(boolean completed) {
+        isCompleted = completed;
+    }
+
     @Override
     public String toString() {
         return "MyTasks{" +
                 "keyid=" + keyid +
-                ", tasks='" + text+ '\'' +
+                ", text='" + text + '\'' +
                 ", importance=" + importance +
                 ", subjid=" + subjid +
                 ", userid=" + userid +
                 ", time=" + time +
+                ", isCompleted=" + isCompleted +
                 '}';
     }
 }
--- app/src/main/java/data/myTasksTable/MyTasksQuery.java
+++ app/src/main/java/data/myTasksTable/MyTasksQuery.java
@@ -1,15 +1,72 @@
 package data.myTasksTable;
 
 import androidx.room.Dao;
+import androidx.room.Delete;
 import androidx.room.Insert;
 import androidx.room.Query;
+import androidx.room.Update;
 
 import java.util.List;
 
-@Dao //واجهه استعلامات على جدول معطيات
+/**
+ * واجهه استعلامات على جدول المعطيات
+ */
+@Dao
 public interface MyTasksQuery {
+    /**
+     *اعاده جميع معطيات جدول المهمات
+     * @return *اعاده قائمه من المهمات
+     */
     @Query("SELECT * FROM mytasks")
     List<MyTasks> getallTasks();
+
+    /**
+     * قائمه من المهمات حسب رقم المستخدم بترتيب او حسب اقامه انتاج المهمه
+     * @param userId_pp رقم الهويه الprimaryKey
+     * @return * اعادة  قائمه من المهمات حسب رقم المستخدم بترتيب او حسب اقامه انتاج المهمه
+     */
+
+@Query( "SELECT * FROM MyTasks WHERE userid=:userId_pp " + "ORDER BY time DESC")
+List<MyTasks> getallTasksOrderBYuserid(long userId_pp);
+
+    /**
+     * قائمه من المهمات الغير مكتمله بترتيب او حسب الاهمية
+     * @param userid_p رقم الهويه ال primarykey
+     * @param iscompleted_p المهمات غي      ر مكتمله
+     * @return
+     */
+    @Query("SELECT * FROM Mytasks WHERE userid=:userid_p AND iscompleted=:iscompleted_p  " + "ORDER BY importance DESC" )
+    List<MyTasks> getalltasksnotcompleted(long userid_p, boolean iscompleted_p);
+
+
+    /**
+     *  ادخال مهمات
+     * @param t  مجموعه مهمات
+     */
     @Insert
     void insertTask(MyTasks...t); // ثلاثة نقاط تعني ادخال مجموعه
+
+    /**
+     * تعديل المهمات
+     * @param tasks مجموعه لتعديل المهمات (التعديل حسب المفتاح الرئيسي)
+     */
+    @Update
+    void UpdateTask(MyTasks... tasks);
+
+    /**
+     * حذف  المهمات او مهمات
+     * @param tasks حذف المهمات (حسب المفتاح الرئيسي)
+     */
+
+    @Delete
+    void deletTask(MyTasks... tasks);
+
+    @Query("DELETE FROM  Mytasks WHERE keyid=:kid  ")
+    void deletTask(long  kid);
+
+
+
+
+
 }
+
