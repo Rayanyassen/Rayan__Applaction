fa1f355d92d1617cb7660d10457e9620023961bd
Rayan <rayanyassen23@gmail.com>
2023-10-30 14:13:49 +0200

    Rayan22

---
+++ app/src/main/java/Rayan/Yassen/myapplication/AddTaskActivty.java
@@ -0,0 +1,14 @@
+package Rayan.Yassen.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddTaskActivty extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_task_activty);
+    }
+}
--- app/src/main/java/Rayan/Yassen/myapplication/sing_out.java
+++
@@ -1,6 +0,0 @@
-package Rayan.Yassen.myapplication;
-
-import android.app.Activity;
-
-public class sing_out extends Activity {
-}
