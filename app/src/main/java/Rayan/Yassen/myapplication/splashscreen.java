22c54ade328ef6a6364420ec443b06fc174aabfe
Rayan <rayanyassen23@gmail.com>
2023-10-31 12:16:11 +0200

    Rayan22

--- app/src/main/java/Rayan/Yassen/myapplication/splashscreen.java
+++ app/src/main/java/Rayan/Yassen/myapplication/splashscreen.java
@@ -1,12 +1,19 @@
 package Rayan.Yassen.myapplication;
 
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import java.util.List;
+import java.util.logging.Handler;
+import java.util.logging.LogRecord;
 
 import data.AppDataBase;
 import data.SubjectTable.MySubject;
@@ -14,32 +21,66 @@ import data.SubjectTable.MySubjectQuery;
 
 
 public class splashscreen extends AppCompatActivity {
+    //spnr1 تعريف صفه الكائن المرئي
+    private Spinner spnrSubject;
+    
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splashscreenactivty);
+
         Log.d("yassen", "onCreate: ");
-        Toast.makeText(this, "onCreate:",Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "onCreate:", Toast.LENGTH_SHORT).show();
         //بناء قاعدة بيانات وارجاع مؤشر عليها 1
-        AppDataBase db= AppDataBase.getDB(getApplicationContext()) ;
+        AppDataBase db = AppDataBase.getDB(getApplicationContext());
         //مؤشر لكائن عمليات لجدول  2
-        MySubjectQuery subjectQuery=db.getMySubjectQuery();
+        MySubjectQuery subjectQuery = db.getMySubjectQuery();
         //بناء كائن من نوع الجدول و تحديد قيم الصفات 3
         MySubject s1 = new MySubject();
         s1.setTitle("Math");
-        MySubject s2= new MySubject();
-        s2.Title="Computers";
+        MySubject s2 = new MySubject();
+        s2.Title = "Computers";
         //اضافه كائن للجدول 4
         subjectQuery.insert(s1);
         subjectQuery.insert(s2);
         //فحص هل تم  حفظ ما سبق 5
         // استخراج وطباعة جميع معطيات الجدول المواضيع
-        List<MySubject> allSubjects=subjectQuery.getAll();
-        for (MySubject s: allSubjects) {
-            Log.d("rayan",s.Title);
-            Toast.makeText(this,"OnCreate:",Toast.LENGTH_SHORT).show();       }
+        List<MySubject> allSubjects = subjectQuery.getAll();
+        for (MySubject s : allSubjects) {
+            Log.d("rayan", s.Title);
+            Toast.makeText(this, "OnCreate:", Toast.LENGTH_SHORT).show();
+        }
+        Handler h = new Handler() {
+            @Override
+            public void publish(LogRecord record) {
+
+            }
+
+            @Override
+            public void flush() {
+
+            }
+
+            @Override
+            public void close() throws SecurityException {
+
+            }
+        };
+        Runnable r = new Runnable() {
+            @Override
+            public void run() {
+                //to open new activity from current to next
+                Intent i = new Intent(splashscreen.this, SignIn.class);
+                startActivity(i);
+                //to close current activity
+                finish();
+            }
+        };
+
     }
+
+
 
 
     @Override
