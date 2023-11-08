697807ff82a95e09567ad56d0a0d219459de4d7b
Rayan <rayanyassen23@gmail.com>
2023-11-08 13:51:53 +0200

    Rayan22

--- app/src/main/java/Rayan/Yassen/myapplication/SignIn.java
+++ app/src/main/java/Rayan/Yassen/myapplication/SignIn.java
@@ -2,7 +2,9 @@ package Rayan.Yassen.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class SignIn extends AppCompatActivity {
 
@@ -10,5 +12,30 @@ public class SignIn extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+    }
+    public void onclickGo1(View v)
+    {
+        Intent i= new Intent(SignIn.this, Sign_up.class);
+        startActivity(i);
+        //to close current activity
+        finish();
+    }
+    public void onclickGo2(View v)
+    {
+        Intent i= new Intent(SignIn.this, Sign_up.class);
+        startActivity(i);
+        //to close current activity
+        finish();
+    }
+
+    public void onclickGo3(View v)
+    {
+        Intent i= new Intent(SignIn.this,MainActivity2.class);
+        startActivity(i);
+        //to close current activity
+        finish();
     }
 }
+
+
--- app/src/main/java/Rayan/Yassen/myapplication/splashscreen.java
+++ app/src/main/java/Rayan/Yassen/myapplication/splashscreen.java
@@ -6,13 +6,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.Toast;
 
 import java.util.List;
-import java.util.logging.Handler;
 import java.util.logging.LogRecord;
 
 import data.AppDataBase;
@@ -51,34 +51,22 @@ public class splashscreen extends AppCompatActivity {
             Log.d("rayan", s.Title);
             Toast.makeText(this, "OnCreate:", Toast.LENGTH_SHORT).show();
         }
-        Handler h = new Handler() {
-            @Override
-            public void publish(LogRecord record) {
+        Handler h=new Handler();
+        Runnable r=new Runnable() {
 
-            }
-
-            @Override
-            public void flush() {
-
-            }
-
-            @Override
-            public void close() throws SecurityException {
-
-            }
-        };
-        Runnable r = new Runnable() {
-            @Override
-            public void run() {
+            public void run()
+            {
                 //to open new activity from current to next
-                Intent i = new Intent(splashscreen.this, SignIn.class);
+                Intent i= new Intent(splashscreen.this, SignIn.class);
                 startActivity(i);
                 //to close current activity
                 finish();
             }
         };
+        h.postDelayed(r,3000);
 
-    }
+
+}
 
 
 
